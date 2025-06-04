fd3761be70d3c14274728414e58903cbccfa93d9
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2025-06-04 18:25:23 +0200

    fix: prevent action from running forever

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ cd ~/mp3-player-api
 source venv/bin/activate
 pip install -r requirements.txt
 fuser -k 8000/tcp || true
-nohup uvicorn server:app --host 0.0.0.0 --port 8000 &
+nohup uvicorn server:app --host 0.0.0.0 --port 8000 > uvicorn.log 2>&1 < /dev/null &
